e6463d2c2f924ef4bb9660f2874d8ef8b0766810
emperorbj <cwaku96@gmail.com>
2026-06-05 00:45:35 +0100

    chore:configured unit test and e2e test libraries

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  testEnvironment: "jest-environment-jsdom",
+
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+
+  collectCoverageFrom: [
+    "**/*.{ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!**/.next/**",
+  ],
+};
+
+export default createJestConfig(customJestConfig);
