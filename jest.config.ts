c0fbe8dce0d41776d33b3b8690e5f505d7f32258
Aaron Attah <aoahorizon@gmail.com>
2026-06-07 01:36:24 +0100

    fix: resolve Jest config import resolution and add missing types

--- jest.config.ts
+++ jest.config.ts
@@ -1,26 +1,21 @@
-import nextJest from "next/jest";
-
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
 const createJestConfig = nextJest({
-  dir: "./",
+  dir: './',
 });
 
-const customJestConfig = {
-  testEnvironment: "jest-environment-jsdom",
+const customJestConfig: Config = {
+  testEnvironment: 'jest-environment-jsdom',
 
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
+    '^@/(.*)$': '<rootDir>/$1',
   },
 
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
 
-  collectCoverageFrom: [
-    "**/*.{ts,tsx}",
-    "!**/*.d.ts",
-    "!**/node_modules/**",
-    "!**/.next/**",
-  ],
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/*.d.ts', '!**/node_modules/**', '!**/.next/**'],
 };
 
 export default createJestConfig(customJestConfig);
